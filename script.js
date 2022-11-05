3218cc02b1038f708b0daa0161423e469b79a20b
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2022-11-05 22:55:30 +0300

    initialommit

---
+++ script.js
@@ -0,0 +1,120 @@
+/* INPUT BILL */
+const bill = document.querySelector("#bill");
+
+/* INPUT CUSTOM */
+const custom = document.querySelector('#custom')
+
+/* INPUT NUMBER OF PEOPLE */
+const noOfPeople = document.querySelector('#nopeople')
+
+/*BUTTON 5%*/
+const five = document.querySelector(".five");
+
+/*BUTTON 10%*/
+const ten = document.querySelector(".ten");
+
+/*BUTTON 15%*/
+const fifteen = document.querySelector(".fifteen");
+
+/*BUTTON 25%*/
+const twentyfive = document.querySelector(".twentyfive");
+
+/*BUTTON 50%*/
+const fifty = document.querySelector(".fifty");
+
+/*BUTTON RESET*/
+const reset = document.querySelector('.reset');
+
+/* WYNIK TIPu */
+const tipResult = document.querySelector('.tip-result');
+
+/*WYNIK TOTAL */
+const totalResult = document.querySelector('.total-result');
+
+/* CONTAINER NA WYNIK */
+let result = [];
+/* CONTAINER NA TIP */
+let tip = [];
+
+
+
+
+addValue = (buttonName, percent) => {
+    buttonName.style.backgroundColor = "hsl(172, 67%, 45%)"
+    let percentToGet = percent;
+    const amount = parseInt(bill.value) 
+    let percentOfValue = (percentToGet / 100) * amount; 
+    result = amount + percentOfValue;
+    tip = percentOfValue;
+    custom.disabled = true;
+}
+
+addFive = () => {
+    addValue(five, 5);
+    ten.disabled = true;
+    fifteen.disabled = true;
+    twentyfive.disabled = true;
+    fifty.disabled = true;
+}
+
+addTen = () => {
+    addValue(ten, 10);
+    five.disabled = true;
+    fifteen.disabled = true;
+    twentyfive.disabled = true;
+    fifty.disabled = true;
+}
+
+addFifteen = () => {
+    addValue(fifteen, 15);
+    five.disabled = true;
+    ten.disabled = true;
+    twentyfive.disabled = true;
+    fifty.disabled = true;
+}
+
+addTwentyFive = () => {
+    addValue(twentyfive, 25);
+    five.disabled = true;
+    ten.disabled = true;
+    fifteen.disabled = true;
+    fifty.disabled = true;
+}
+
+addFifty = () => {
+    addValue(fifty, 50);
+    five.disabled = true;
+    ten.disabled = true;
+    fifteen.disabled = true;
+    twentyfive.disabled = true;
+    
+}
+
+custom.oninput = () =>{
+    let percentToGet = parseInt(custom.value);
+    const amount = parseInt(bill.value) 
+    let customValue = (percentToGet / 100) * amount; 
+    result = amount + customValue;
+    tip = customValue;
+    five.disabled = true;
+    ten.disabled = true;
+    fifteen.disabled = true;
+    twentyfive.disabled = true;
+    fifty.disabled = true;
+    }
+
+noOfPeople.oninput = () => {
+    const numberOfPeople = parseInt(noOfPeople.value)
+    const fullAmount = result;
+    let totalPerPerson = fullAmount / numberOfPeople
+    let tipPerPerson = tip / numberOfPeople
+    totalResult.innerText = '$' + (totalPerPerson).toFixed(1)
+    tipResult.innerText = '$' + (tipPerPerson).toFixed(1)
+
+}
+
+five.addEventListener('click', addFive)
+ten.addEventListener('click', addTen)
+fifteen.addEventListener('click', addFifteen)
+twentyfive.addEventListener('click', addTwentyFive)
+fifty.addEventListener('click', addFifty)
